89d2731d14b916c64b9f6df8be586cc7c94b895d
quiz22 <quiz22@qq.com>
2024-04-23 10:40:15 +0800

    渲染

---
+++ 0423/render/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import {reactive, ref} from 'vue';
+  const type = ref('B')
+  const list = reactive(['曹老板','孙十万','大耳贼']);
+</script>
+
+<template>
+  <h1>小明的学习评定等级为：</h1>
+  <P v-if="type === 'A'">优秀</P>
+  <P v-else-if="type === 'B'">良好</P>
+  <P v-else>差</P>
+  <button @click="type='A'">优秀</button>
+  <button @click="type='B'">良好</button>
+  <button @click="type='C'">差</button>
+  <hr>
+  <p v-for="(item,index) in list">
+    索引是：{{index}}，元素的内容是{{item}}
+  </p>
+</template>
+
+<style scoped>
+
+</style>
