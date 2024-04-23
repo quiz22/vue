0f7b51f7cd1770f185728d5a6f0d71362441c4ea
quiz22 <quiz22@qq.com>
2024-04-23 10:30:01 +0800

    组件

--- 0422/demo/src/components/demo.vue
+++ 0422/demo/src/components/demo.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="demo">Demo组件</div>
+<div class="demo">{{title}}</div>  
+<div class="demo">{{obj.title}}</div>
+<input type="text" name="" id="" v-model="username">
+<div>{{username}}</div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import {reactive,ref,toRef} from 'vue';  
+  const arr = ["湖南","长沙"];  
+  let obj = reactive({  
+    title:''
+  });  
+  let {title} = toRef(obj);  
+  console.log({title});
+  let username = ref('admin');
+  
+  let pos = 0;  
+  let tm = setInterval(()=>{  
+    obj.title = arr[pos];  
+    
+    pos++;  
+    if(pos >= arr.length)  
+      clearInterval(tm);  
+  },500);
 </script>
 
 <style>
--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 反正是测试的。这是个md文件,里面随便打
+
 git config --global user.name 'xxx'
 
 git config --global user.email "123@qq.com"
 
-git clone https://github.com/quiz22/test.git
+git clone https://github.com/quiz22/vue.git
 
 git config --global http.sslVerify “false”
+
+安装vue
+
+npm create vue@latest
+
+安装vite框架
+npm create vite@latest
+
+npm install
+
+npm run dev
