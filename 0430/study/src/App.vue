6415919907daa8ad4a5038a8581872e333d4e17d
quiz22 <quiz22@qq.com>
2024-05-06 15:59:48 +0800

    渲染组件

--- 0430/study/src/App.vue
+++ 0430/study/src/App.vue
@@ -1,17 +1,100 @@
 <script setup>
- import { ref } from 'vue'
- const list = ref([
- { 
-  id: '1',
-  subject: 'Vue.js 前端实战开发',
-  content: '学习指令，例如 v-if、v-for、v-model 等',
-  place: '自习室',
-  status: false,
-  },
-  ])
+import { ref } from "vue";
+const list = ref([
+  {
+    id: "1",
+    subject: "Vue.js 前端实战开发",
+    content: "学习指令，例如 v-if、v-for、v-model 等",
+    place: "自习室",
+    status: false
+  }
+]);
+let remove = (id, status) => {
+  if (status) {
+    list.value = list.value.filter(item => item.id != id);
+  } else {
+    alert("请完成该学习计划后再进行删除操作！");
+  }
+};
+let subject = ref("");
+let content = ref("");
+let nextId = ref("");
+let selectedOption = ref("自习室");
+let options = ref([
+  { placeCode: 0, place: "自习室" },
+  { placeCode: 1, place: "图书馆" },
+  { placeCode: 2, place: "宿舍" }
+]);
+
+let add = () => {
+  if (subject.value === "") {
+    alert("学习科目为必填项！");
+    return;
+  }
+  nextId.value = Math.max(...list.value.map(item => item.id)) + 1;
+  const obj = {
+    id: nextId.value,
+    subject: subject.value,
+    content: content.value,
+    place: selectedOption.value,
+    status: false
+  };
+  list.value.push(obj);
+  subject.value = "";
+  content.value = "";
+  selectedOption.value = "自习室";
+};
 </script>
 
 <template>
+  <div class="card">
+    <!-- 标题区域 -->
+    <div class="card-header">学习计划表</div>
+    <!-- 提交区域 -->
+    <div class="card-body">
+      <form @submit.prevent="add">
+        <div class="row g-4">
+          <!-- 学习科目 -->
+          <div class="col-auto">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1">学习科目</span>
+              <input type="text" class="form-control" placeholder="请输入学习科目" v-model.trim="subject" />
+            </div>
+          </div>
+          <!-- 学习任务 -->
+          <div class="col-auto">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1">学习内容</span>
+              <textarea
+                class="form-control"
+                v-model.trim="content"
+                placeholder="请输入学习内容"
+                :style="{ height: '32px' }"
+              ></textarea>
+            </div>
+          </div>
+          <!-- 学习地点 -->
+          <div class="col-auto">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1">学习地点</span>
+              <select class="form-select form-select-sm" v-model="selectedOption">
+                <option
+                  v-for="option in options"
+                  :value="option.place"
+                  :key="option.placeCode"
+                >{{ option.place }}</option>
+              </select>
+            </div>
+          </div>
+          <!-- 添加按钮 -->
+          <div class="col-auto">
+            <button type="submit" class="btn btn-primary">添加</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
   <table class="table table-striped table-hover table-bordered">
     <thead>
       <tr>
@@ -23,7 +106,6 @@
         <th scope="col">操作</th>
       </tr>
     </thead>
-
     <tbody>
       <tr v-for="item in list" :key="item.id">
         <td>{{ item.id }}</td>
@@ -32,13 +114,19 @@
         <td>{{ item.place }}</td>
         <td>
           <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" v-model="item.status" />
+            <input
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+              v-model="item.status"
+            />
             <label class="form-check-label" for="flexSwitchCheckDefault" v-if="item.status">已完成</label>
             <label class="form-check-label" for="flexSwitchCheckDefault" v-else>未完成</label>
           </div>
         </td>
         <td>
-          <a href="javascript:;">删除</a>
+          <a href="javascript:;" @click="remove(item.id, item.status)">删除</a>
         </td>
       </tr>
     </tbody>
