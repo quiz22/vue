e4a42803cd144e09c65f45e5e08d126e6ba171dd
quiz22 <quiz22@qq.com>
2024-05-13 15:48:14 +0800

    渲染组件

---
+++ 0513/demo/src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>子组件</h3>
+        <p>{{title}}</p>
+        <p>{{count +1 }}</p>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        title: String,
+        count: {required: true}
+    })
+</script>
+
+<style scoped>
+
+
+</style>
