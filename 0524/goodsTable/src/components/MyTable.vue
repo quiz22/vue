dfa482c3cd8deb0a199cf875e824ceb5f0ab04ef
quiz22 <quiz22@qq.com>
2024-05-27 15:27:21 +0800

    商品管理项目

---
+++ 0524/goodsTable/src/components/MyTable.vue
@@ -0,0 +1,28 @@
+<template>
+    <table class="table table-striped table-bordered">
+        <!-- 表格标题区域 -->
+        <thead>
+            <tr>
+                <slot name="header"></slot>
+            </tr>
+        </thead>
+        <!-- 表格主体区域 -->
+        <tbody>
+            <tr v-for="(item,idx) in goodsList" :key="item.id">
+                <slot name="body" :row="item" :idx="idx"></slot>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+const props = defineProps({
+    goodsList: {
+        type: Array,
+        required: true,
+        default: []
+    }
+});
+</script>
+
+<style scoped></style>
