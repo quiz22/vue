faf534b1a4a8cb4032993d7d4ca5c9a1a0dcf896
quiz22 <quiz22@qq.com>
2024-05-27 15:23:13 +0800

    路由模块

---
+++ 0527/demoRouter/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <h1>根组件APP</h1>
+  <route-link to="/home">首页</route-link>
+  <route-link to="/about">关于</route-link>
+  <route-view></route-view>
+</template>
+
+<style scoped>
+
+</style>
