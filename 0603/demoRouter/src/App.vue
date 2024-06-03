575951734c08c81d88f5909c4712c88553ee326a
quiz22 <quiz22@qq.com>
2024-06-03 15:16:31 +0800

    element-plus

---
+++ 0603/demoRouter/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <hr>
+  <h1>Element PLUS</h1>
+  <hr>
+  <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">primary</el-button>
+      <el-button type="success">success</el-button>
+      <el-button type="info">info</el-button>
+      <el-button type="warning">warning</el-button>
+      <el-button type="danger">danger</el-button>
+  </el-row>
+  <el-row class="mb-4">
+      <el-button plain>Default</el-button>
+      <el-button plain type="primary">primary</el-button>
+      <el-button plain type="success">success</el-button>
+      <el-button plain type="info">info</el-button>
+      <el-button plain type="warning">warning</el-button>
+      <el-button plain type="danger">danger</el-button>
+  </el-row>
+  <hr>
+  <h1>vant</h1>
+  <hr>
+</template>
+
+<style scoped>
+
+</style>
